ad98dc6d448d12014e25b7e7c8e24442fe600484
KP-Kepra <kevinpr153@gmail.com>
2015-10-12 10:53:16 +0700

    asdfasdf

---
+++ scoring.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="css.css" rel="stylesheet" type="text/css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!--[if lt IE 9]>
+  	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+<?php  
+	error_reporting(0);
+	$nama = $_POST["nama"];
+?>	
+<header>
+	<div class="name">WELCOME, <?php echo $nama; ?>. <br> <?php echo date("d/m/Y"); ?> </div>
+	<ul>
+        <li><a href="main.php" title="Menu 1">Main Menu</a></li>
+        <li><a href="reports.php" title="Menu 2">Reports</a></li>
+		<li><a href="profile.php" title="Menu 3">Profile</a></li>
+    </ul> 
+</header>
+<section class="scoring-temp">
+	<section class="scoring-report">
+		tes
+	</section>
+	<section class="scoring-teacher">
+		tes
+	</section>
+	<section class="scoring-tab">
+		tes
+	</section>
+</section>
+<footer>
+	FOOTER
+</footer>
+</body>
+</html>
