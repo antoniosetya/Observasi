b4ca00bec2ee2d7ac0071f87debf4f84e2d80c14
antoniosetya <antonio_setya@hotmail.com>
2015-10-12 10:16:45 +0700

    Nambahin CSS dikit

--- main.php
+++ main.php
@@ -4,6 +4,9 @@
 	<meta charset="utf-8">
 	<link href="css.css" rel="stylesheet" type="text/css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!--[if lt IE 9]>
+  	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 </head>
 <body>
 	
