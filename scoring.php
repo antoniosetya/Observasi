2cafbb0d860b090193f8fc7f74fc8eb376613961
KP-Kepra <kevinpr153@gmail.com>
2015-10-12 14:26:59 +0700

    scoring 50%

--- scoring.php
+++ scoring.php
@@ -3,10 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<link href="css.css" rel="stylesheet" type="text/css" />
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<!--[if lt IE 9]>
-  	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
 </head>
 <body>
 <?php  
@@ -22,20 +18,39 @@
     </ul> 
 </header>
 <section class="scoring-temp">
-	<li>
-		tes
-	</li><!--
+	<ul id="scoring-report" style="width : 20%">
+		<li id="1" onClick="score_report(this.id)">Report 1</li>
+		<li id="2" onClick="score_report(this.id)">Report 2</li>
+	</ul><!--
 	
-	--><li>
-		tes
-	</li><!--
+	--><ul class="scoring-teacher" style="width : 25%; background : blue;">
+			<li id="1" onClick="score_teacher(this.id)">Teacher 1</li>
+			<li id="2" onClick="score_teacher(this.id)">Teacher 2</li>
+	</ul><!--
 	
-	--><li>
-		tes
-	</li>
+	--><ul class="rate" style="width : 55%">
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+	</ul>
 </section>
 <footer>
 	FOOTER
 </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript">
+function score_report(id)
+{
+	$(".scoring-teacher").load("datadummy.html #teacher"+id);
+}
+</script>
+<script type="text/javascript">
+function score_teacher(id)
+{
+	$(".rate").load("datadummy.html #rate"+id);
+}
+</script>
 </body>
 </html>
